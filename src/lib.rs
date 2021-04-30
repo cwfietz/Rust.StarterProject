480ae396c4848aa779404e9aac37b83a8692b058
cwfietz <43331107+cwfietz@users.noreply.github.com>
2021-04-30 19:46:57 +0000

    post mob work.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
-pub fn hello() -> &'static str {
-    "Hello, World!"
+pub enum Health {
+    Alive,
+    Dead
+}
+
+pub fn is_alive_next_round(live_neighbors : i32, is_alive: Health) -> Health {
+    match live_neighbors {
+        3 => Health::Alive,
+        2 => is_alive,
+        _ => Health::Dead
+    }
 }
--- tests/hello-world.rs
+++ tests/hello-world.rs
@@ -1,4 +1,36 @@
+use Hello
+
 #[test]
-fn test_hello_world() {
-    assert_eq!("Hello, World!", hello_world::hello());
+fn test_lonely_live_cell_is_doomed() {
+    assert_eq!(hello_world::is_alive_next_round(0, Health::Alive), Health::Dead);
 }
+
+#[test]
+fn test_live_cell_with_2_live_neighbors_stay_alive() {
+    assert_eq!(hello_world::is_alive_next_round(2, Health::Alive), Health::Alive);
+}
+
+
+#[test]
+fn test_lonely_dead_cell_stays_dead() {
+    assert_eq!(hello_world::is_alive_next_round(0, Health::Dead), Health::Dead);
+}
+
+#[test]
+fn test_happy_cell_becomes_alive(){
+    assert_eq!(hello_world::is_alive_next_round(3, Health::Dead ), Health::Alive);
+}
+
+#[test]
+fn test_crowded_cell_dies(){
+    assert_eq!(hello_world::is_alive_next_round(4,Health::Alive), Health::Dead);
+    assert_eq!(hello_world::is_alive_next_round(5,Health::Alive), Health::Dead);
+    assert_eq!(hello_world::is_alive_next_round(8,Health::Alive), Health::Dead);
+}
+
+#[test]
+fn test_dead_cell_stays_dead(){
+    assert_eq!(hello_world::is_alive_next_round(2, Health::Dead), Health::Dead);
+}
+
+
